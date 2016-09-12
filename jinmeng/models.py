a6ff6e82d3fbbd2ec18398d0f17269abf0295738
NanNan <doudou@NanNandeMacBook-Air.local>
2016-09-12 17:56:45 +0800

    金盟页面显示图表

--- jinmeng/models.py
+++ jinmeng/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
-
 from django.db import models
+import time
 
 # Create your models here.
 
@@ -9,5 +9,8 @@ class HK_jiaoyisuo(models.Model):
     data1 = models.IntegerField()
     data2 = models.IntegerField()
     data3 = models.CharField(max_length=4)
+    save_data = models.DateTimeField(default=time.time())
     class Meta:
         db_table = 'hk_jiaoyisuo'
+    def __str__(self):
+        return str(self.data2)
--- jinmeng/views.py
+++ jinmeng/views.py
@@ -4,5 +4,21 @@ from .models import HK_jiaoyisuo
 # Create your views here.
 def jinmeng(request):
     HK = HK_jiaoyisuo.objects.all()
+
+    # time_hk = HK_jiaoyisuo.objects.values('time_hk')[0:10]
+    # time_hk_html = list()
+    # for i in time_hk:
+    #     time_hk_html.append(str(i['time_hk'].strftime(' %H:%M')))
+    #
+    # data1 = HK_jiaoyisuo.objects.values('data1')[0:10]
+    # data1_html = list()
+    # for i in data1:
+    #     data1_html.append(int(str(i['data1'])))
+    #
+    # data2 = HK_jiaoyisuo.objects.values('data2')[0:10]
+    # data2_html = list()
+    # for i in data2:
+    #     data1_html.append(int(str(i['data2'])))
+
     return render(request, 'jinmeng.html', locals())
 
